aae16d48456edddc5c7e328f970d5fcc5e491c81
KennyP <kenny.peeters@cubis.be>
2016-06-06 13:57:38 +0200

    Ik moet iets zeggen, maar ik wil niet !

--- Cubis.Gauge/res/js/component.js
+++ Cubis.Gauge/res/js/component.js
@@ -4,17 +4,10 @@ sap.designstudio.sdk.Component.subclass("com.sap.sample.scngauge.gauge", functio
 	var me = this;
 	 
 	//Properties
-	me._colorCode = 'green';
-	me._innerRad = 0.0;
-	me._outerRad = 0.0;
-	me._endAngleDeg = 90.0;
-	me._startAngleDeg = -120.0;
-	me._paddingTop = 0.0;
-	me._paddingBottom = 0.0;
-	me._paddingLeft = 0.0;
-	me._paddingRight = 0.0;
-	me._offsetLeft = 0.0;
-	me._offsetDown = 0.0;   
+	me._textcolorCode = 'orange';
+	me._text = 'test';
+	me._textcolorCodeFill = 'black';
+	me._percentage = 10;
 	
 	me.init = function() 
 	{
@@ -23,48 +16,44 @@ sap.designstudio.sdk.Component.subclass("com.sap.sample.scngauge.gauge", functio
 	
 	me.redraw = function() 
 	{
-		 var myDiv = me.$()[0];
-		 
-		  // Clear any existing gauges.  We'll redraw from scratch
-		  d3.select(myDiv).selectAll("*").remove();
-		 
-		  var vis = d3.select(myDiv).append("svg:svg").attr("width", "100%").attr("height", "100%");
-		  var pi = Math.PI;
-		 
-		  // Find the larger left/right padding
-		  var lrPadding = me._paddingLeft + me._paddingRight;
-		  var tbPadding = me._paddingTop + me._paddingBottom;
-		  var maxPadding = lrPadding;
-		  if (maxPadding < tbPadding){
-		  maxPadding = tbPadding
-		  }
-		 
-		  me._outerRad = (me.$().width() - 2*(maxPadding))/2;
-		 
-		  //The offset will determine where the center of the arc shall be
-		  me._offsetLeft = me._outerRad + me._paddingLeft;
-		  me._offsetDown = me._outerRad + me._paddingTop;
-		 
-		  var arcDef = d3.svg.arc()
-		  .innerRadius(me._innerRad)
-		  .outerRadius(me._outerRad)
-		  .startAngle(me._startAngleDeg * (pi/180)) //converting from degs to radians
-		  .endAngle(me._endAngleDeg * (pi/180)); //converting from degs to radians
-		 
-		 
-		  var guageArc = vis.append("path")
-		     .style("fill", me._colorCode)
-		     .attr("width", me.$().width()).attr("height", me.$().height()) // Added height and width so arc is visible
-		     .attr("transform", "translate(" + me._offsetLeft + "," + me._offsetDown + ")")
-		     .attr("d", arcDef);
+		var my2Div = me.$()[0];
+		
+		// Clear any existing gauges.  We'll redraw from scratch
+		d3.select(my2Div).selectAll("*").remove();
+		
+		var svgText = d3.select(my2Div)
+		  .append("svg:svg")
+		  .attr("width", "100%")
+		  .attr("height", "100%");		
+		
+		var svgText2 = d3.select(my2Div)
+		.append("svg:svg")
+		.attr("width", "100%")
+		.attr("height", "100%");
+		
+		alert(me._percentage);
+		
+		// append text to svg
+		svgText.append("text")
+		.style("font-size", me._percentage + "px")
+		.style("fill", me.textcolorCode)
+		.style("font-family", "Verdana")
+		.attr("x", 10)
+		.attr("y", 10)
+		.text(me._text);
+		
+		svgText2.append("text")
+		.style("font-size", me._percentage + "px")
+		.style("fill", me.textcolorCodeFill)
+		.style("font-family", "Verdana")
+		.attr("x", 10)
+		.attr("y", 10)
+		.text(me._text);
 	};
-	
-	
-	
-	
+		
 	//Getters and Setters
 	// Colorcode
-	me.colorCode = function(value) 
+	me.textcolorCode = function(value) 
 	{
 	  if (value === undefined)
 	  {
@@ -78,72 +67,27 @@ sap.designstudio.sdk.Component.subclass("com.sap.sample.scngauge.gauge", functio
 	  }
 	};
 	
-	//Inner radius
-	me.innerRad = function(value) 
+	me.textcolorCodeFill = function(value) 
 	{
-		
 	  if (value === undefined)
 	  {
-		  return me._innerRad;
+		  return me._colorCodeFill;
 	  } 
 	  else
-	  {	 
-		  var isValid = me.validateRadii(value, me._outerRad);
-		  if (isValid === false)
-		  {
-			  alert("Warning!  The gauge arc can't have a small inner radius than outer!  Inner Radius must be equal to or less than " + me._outerRad);
-			  alert("Please decrease the inner radius, or increase the size of the control.  Height & width (including subtraction for padding) must me at least twice as large as Internal Radius!");
-		  }
-		  else
-		  {
-			  me._innerRad = value;
-			  me.redraw();
-		  }
-		  return this;
+	  {
+		  me._colorCodeFill = value;
+		  me.redraw();
+		  return me;
 	  }
 	};
 	
-	//Validate the Inner and Outer Radii
-	me.validateRadii = function(inner, outer) 
-	{
-		if (inner <= outer) 
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	};
-	
-	// change angle start & end
-	me.endAngleDeg = function(value) 
-	{
-		if (value === undefined) 
-		{
-			return me._endAngleDeg;
-		}
-		else
-		{
-			me._endAngleDeg = value;
-			me.redraw();
-			return this;
-		}
-	};
- 
-	me.startAngleDeg = function(value) 
-	{
-		if (value === undefined) 
-		{
-			return me._startAngleDeg;
-		}
-		else
-		{
-			me._startAngleDeg = value;
-			me.redraw();
-			return this;
-		}
-	};
-
+	me.percentage = function(value) {
+	    if (value === undefined) {
+	    return me._percentage;
+	    } else {
+	    me._percentage = value;
+	    return this;
+	    }
+	  };
 	
 });
