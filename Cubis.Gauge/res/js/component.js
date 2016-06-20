ef3808aa9b780ddbf9376e1c4cf66b2dbf821cbe
KennyP <kenny.peeters@cubis.be>
2016-06-20 16:08:28 +0200

    This works !

--- Cubis.Gauge/res/js/component.js
+++ Cubis.Gauge/res/js/component.js
@@ -29,45 +29,51 @@ sap.designstudio.sdk.Component.subclass("com.sap.sample.scngauge.gauge", functio
 		
 		if (me._percentage === null || me._percentage === "") {
 			me._size = "50";
+			me._text = "Cubis";
+			
+			var svgText = d3.select(my2Div)
+			.append("svg:svg")
+			.attr("width", "100%")
+			.attr("height", "100%");
+	
+			//append text to svg
+			svgText.append("text")
+			.style("font-size", me._size + "px")
+			.style("font-family", "Verdana")
+			.attr("x", 10)
+			.attr("y", 10)
+			.text(me._text);
 		}
 		else {
-			me._size = me._percentage.formattedData[0];
-		}
-		
-		var svgText = d3.select(my2Div)
-		.append("svg:svg")
-		.attr("width", "100%")
-		.attr("height", "100%");
-		
-		// append text to svg
-		svgText.append("text")
-		.style("font-size", me._size + "px")
-		.style("font-family", "Verdana")
-		.attr("x", 10)
-		.attr("y", 10)
-		.text(me._text);
-		
-	};
-		
-	me.afterUpdate = function(){
-		
-		console.log("afterUpdate method");
-		
-		// syntax "== null" is different from "=== null"
-		// "== null" actually checks both for null and undefined
-		if (me._percentage === null || me._percentage === "") {
-			me._text = "Cubis";
-			me.redraw();
-		}
-		else {	
-			console.log("me._percentage = " + me._percentage);
+			console.log(me._percentage.tuples.length+"");
+			var tuple = me._percentage.tuples;
 			
-			me._tuple = me._percentage.tuples[0];
-			me._text = me._meta_data.dimensions[1].members[me._tuple[0]].text;
-			me.redraw();
+			for(i=0 ; i < me._percentage.tuples.length; i++){
+					
+				me._size = me._percentage.formattedData[i];
+				me._text = me._meta_data.dimensions[1].members[tuple[i][1]].text;
+				console.log(me._text);
+				
+				if(me._meta_data.dimensions[1].members[tuple[i][1]].type !== "RESULT"){
+					var svgText = d3.select(my2Div)
+					.append("svg:svg")
+					.attr("width", "100%")
+					.attr("height", "100%");
+			
+					//append text to svg
+					svgText.append("text")
+					.style("font-size", me._size + "px")
+					.style("font-family", "Verdana")
+					.attr("x", 10)
+					.attr("y", 10)
+					.text(me._text);
+				}
+			}
 		}
+		
+		
 	}
-	
+			
 	me.percentage = function(value) {
 		
 		console.log("percentage method");
@@ -75,8 +81,7 @@ sap.designstudio.sdk.Component.subclass("com.sap.sample.scngauge.gauge", functio
 	    if (value === undefined) {
 	    	return me._percentage;
 	    } else {
-	    	me._percentage = value;
-	    	//me.redraw();
+	    	me._percentage = value;	
 	    	return me;
 	    }
 	  };
@@ -89,7 +94,7 @@ sap.designstudio.sdk.Component.subclass("com.sap.sample.scngauge.gauge", functio
 			return me._meta_data;
 		} else {
 			me._meta_data = value;
-	    	//me.redraw();
+			me.redraw();
 			return me;
 		}
 	};
